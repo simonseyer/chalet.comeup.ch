27d64193ce8d4f97780d4218a402a5924e222ae4
Simon Seyer <simon.seyer@gmail.com>
2020-07-08 22:23:27 +0200

    Implemented internationalization

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 const fileinclude = require('gulp-file-include');
+const i18n        = require('gulp-html-i18n');
+const filter      = require('gulp-filter');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -11,20 +13,30 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('fileinclude', function() {
+gulp.task('html', function() {
+  const fileFilter = filter(['**', '!**/_*.html']);
+
     gulp.src(['src/*.html'])
+      .pipe(fileFilter)
       .pipe(fileinclude())
+      .pipe(i18n({
+        langDir: './src/lang',
+        trace: true,
+        createLangDirs: true,
+        defaultLang: 'de'
+      }))
       .pipe(gulp.dest('./'));
   });
 
 // Static Server + watching scss/html files
-gulp.task('default', gulp.parallel(['sass', 'fileinclude'], function() {
+gulp.task('default', gulp.parallel(['sass', 'html'], function() {
 
     browserSync.init({
         server: "."
     });
 
     gulp.watch("src/scss/**/*.scss", gulp.series('sass'));
-    gulp.watch("src/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch("src/*.html").on('change', gulp.series('html'));
+    gulp.watch("src/lang/**/*.yaml").on('change', gulp.series('html'));
     gulp.watch("*.html").on('change', browserSync.reload);
 }));
